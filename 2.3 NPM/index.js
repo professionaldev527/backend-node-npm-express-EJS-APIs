277967b57ac9cbaa967df7ddc5ae80146fc70719
professionaldev527 <professionaldev527@gmail.com>
2024-08-22 16:39:56 +0530

    2.3 NPM
    Getting started with NPM and install a module called superheroes.

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+// import generateName from "sillyName";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import { randomSuperhero } from "superheroes";
+
+const name = randomSuperhero();
+
+console.log(name); // This will show you the available methods or properties
